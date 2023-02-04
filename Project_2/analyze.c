7210d89d10b79e22cb315491ff2cd2e3d09a61a8
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-02-04 12:35:34 -0500

    Current vlaue with wrong coeff

--- Project_2/analyze.c
+++ Project_2/analyze.c
@@ -7,37 +7,51 @@
 // You can add other functions if you want to perform
 // other calculations independent of these functions.
 
-void data_init( data_t *p_this, double step_size ) {
-  //Initialize dynamic array 
-  p_this = malloc (sizeof (int) * CAPACITY);
-  front = 0;
+void data_init(data_t *p_this, double step_size)
+{
+  // Initialize dynamic array
+  p_this = malloc(sizeof(int) * CAPACITY);
+  p_this->front = 0;
 }
 
 // You may not need this if you do not have a dynamically
 // allocated array.
-void data_destroy( data_t *p_this ) {
+void data_destroy(data_t *p_this)
+{
   // Free data
   free(p_this);
 }
 
-void data_append( data_t *p_this, double new_value ) {
-  // Efficient solution would use mod(n) something  
+void data_append(data_t *p_this, double new_value)
+{
+  // Efficient solution would use mod(n) something
 
-  //Possible solution:
-    // keep track of where front is in array 
-    // mod capacity each time 1 mod 10 = 1 and keep going on
-    // that's the index!
-  p_this->entries_[front % CAPACITY] = new_value;
-  front = front + 1;
+  // Possible solution:
+  //  keep track of where front is in array
+  //  mod capacity each time 1 mod 10 = 1 and keep going on
+  //  that's the index!
+  p_this->entries_[p_this->front % CAPACITY] = new_value;
+  p_this->front = p_this->front + 1;
 }
 
-double data_current( data_t *p_this ) {
+double data_current(data_t *p_this)
+{
   // Find solution a.k.a. coeff for polynomial
-  
-  return 0.0;
+  double coeff[] = {-9.9242e-02,  -6.2844e-02,   8.9371e-02,   1.2130e-01,   1.6396e-01,   1.8743e-01,   1.9842e-01,   1.6447e-01,   1.5546e-01,   8.1669e-02};
+  size_t k;
+
+  double result = 0.0;
+
+  for (k = 0; k < CAPACITY; ++k)
+  {
+    result += coeff[k] * (p_this->entries_[p_this->front % CAPACITY] );
+  }
+
+  return result;
 }
 
-double data_next( data_t *p_this ) {
+double data_next(data_t *p_this)
+{
   // Your implementation here...
   return 0.0;
 }
--- Project_2/analyze.h
+++ Project_2/analyze.h
@@ -3,24 +3,23 @@
 
 #define CAPACITY 10
 
-// Stores where front of array is
-int front;
-
 // Structure declarations
 typedef struct data data_t;
 
 // Function declarations
-void data_init( data_t *p_this, double step_size );
+void data_init(data_t *p_this, double step_size);
 // You may not need this
-void data_destroy( data_t *p_this );
+void data_destroy(data_t *p_this);
 
-void data_append( data_t *p_this, double new_value );
+void data_append(data_t *p_this, double new_value);
 
-double data_current( data_t *p_this );
-double data_next( data_t *p_this );
+double data_current(data_t *p_this);
+double data_next(data_t *p_this);
 
 // Structure definitions
-struct data {
+struct data
+{
   double entries_[CAPACITY];
+  int front;
 };
 #endif
