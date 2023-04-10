45b45904f8ba40bd75ba47a392766e9c09935ffd
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-04-09 20:25:50 -0400

    final final

--- Project_5/main.cpp
+++ Project_5/main.cpp
@@ -3,62 +3,120 @@
 #include <cmath>
 
 int main();
-double range(std::function<double(double)> f,
-             double a,
-             double b,
-             double max_freq);
+
+double range( std::function<double( double )> f,
+              double a,
+              double b,
+              double max_freq );
 // These will change for the tests
-double f1(double x);
-double f2(double x);
-double f3(double x);
+double f1( double x );
+double f2( double x );
+double f3( double x );
 void sort(double &x0, double &x1, double &x2,
           double &f0, double &f1, double &f2);
 
-int main()
-{
-    std::cout.precision(16);
+int main() {
+  std::cout.precision( 16 );
 
-    // These will change  for the tests
-    double max_freq1{8.3 / 2.0 / M_PI};
-
-    for (double a{0.0}; a <= 5.0; ++a)
-    {
-        for (double b{1.0}; b <= 14.0; ++b)
-        {
-            std::cout << "[" << a << ", " << a + b << "]" << std::endl;
-            f1(INFINITY);
-            std::cout << range(f1, a, a + b, max_freq1) << std::endl;
-            std::cout << "Function calls: " << f1(INFINITY) << std::endl;
-            std::cout << range(f1, a, a + b, max_freq1) << std::endl;
-            f1(INFINITY);
-            std::cout << " >> " << range(f1, a, a + b, max_freq1) - range(f1, a, a + b, max_freq1) << std::endl;
-            std::cout << std::endl;
-        }
+  // These will change  for the tests
+  double max_freq1{ 8.3/2.0/M_PI };
+  
+  for ( double a{ 0.0 }; a <= 5.0; ++a ) {
+    for ( double b{ 1.0 }; b <= 14.0; ++b ) {
+      std::cout << "[" << a << ", " << a + b << "]" << std::endl;
+      f1( INFINITY  );
+      std::cout << range( f1, a, a + b, max_freq1 ) << std::endl;
+      std::cout << "Function calls: " << f1( INFINITY  ) << std::endl;
+      std::cout << range( f1, a, a + b, max_freq1 ) << std::endl;
+      f1( INFINITY  );
+      std::cout << " >> " << range( f1, a, a + b, max_freq1 )
+                 - range( f1, a, a + b, max_freq1 ) << std::endl;
+      std::cout << std::endl;
     }
+  }
 
-    return 0;
+  double max_freq2{ 1.0/M_PI };
+
+  for ( double a{ 0.0 }; a <= 5.0; ++a ) {
+    for ( double b{ 1.0 }; b <= 14.0; ++b ) {
+      std::cout << "[" << a << ", " << a + b << "]" << std::endl;
+      f2( INFINITY  );
+      std::cout << range( f2, a, a + b, max_freq2 ) << std::endl;
+      std::cout << "Function calls: " << f2( INFINITY  ) << std::endl;
+      std::cout << range( f2, a, a + b, max_freq2 ) << std::endl;
+      f2( INFINITY  );
+      std::cout << " >> " << range( f2, a, a + b, max_freq2 )
+                 - range( f2, a, a + b, max_freq2 ) << std::endl;
+      std::cout << std::endl;
+    }
+  }
+
+  double max_freq3{ 2.0/M_PI };
+
+    for ( double a{ 0.0 }; a <= 0.0; ++a ) {
+    for ( double b{ 10.0 }; b <= 10.0; ++b ) {
+      std::cout << "[" << a << ", " << a + b << "]" << std::endl;
+      f3( INFINITY  );
+      std::cout << range( f3, a, a + b, max_freq3 ) << std::endl;
+      std::cout << "Function calls: " << f2( INFINITY  ) << std::endl;
+      std::cout << range( f3, a, a + b, max_freq3 ) << std::endl;
+      f3( INFINITY  );
+      std::cout << " >> " << range( f3, a, a + b, max_freq2 )
+                 - range( f3, a, a + b, max_freq2 ) << std::endl;
+      std::cout << std::endl;
+    }
+  }
+
+  return 0;
 }
 
-////////////////////////
-// The function 'f(x) //
+  ////////////////////////
+ // The function 'f(x) //
 ////////////////////////
 
-double f1(double x)
-{
-    static unsigned long count{0};
+double f1( double x ) {
+  static unsigned long count{ 0 };
 
-    if (x == INFINITY)
-    {
-        unsigned long value{count};
-        count = 0;
-        return value;
-    }
-    else
-    {
-        ++count;
-        return std::sin(2.4 * x + 0.8) + 3.5 * std::sin(8.3 * x + 1.5) + 3.7 * std::sin(0.5 * x + 1.2);
-    }
+  if ( x == INFINITY ) {
+    unsigned long value{ count };
+    count = 0;
+    return value;
+  } else {
+    ++count;
+    return 
+            std::sin( 2.4*x + 0.8 )
+      + 3.5*std::sin( 8.3*x + 1.5 )
+      + 3.7*std::sin( 0.5*x + 1.2 );
+  }
 }
+
+double f2( double x ) {
+  static unsigned long count{ 0 };
+
+  if ( x == INFINITY ) {
+    unsigned long value{ count };
+    count = 0;
+    return value;
+  } else {
+    ++count;
+    return std::sin( x ) + std::sin( 2.0*x + 1.0 );
+  }
+}
+
+double f3( double x ) {
+  static unsigned long count{ 0 };
+
+  if ( x == INFINITY ) {
+    unsigned long value{ count };
+    count = 0;
+    return value;
+  } else {
+    ++count;
+    return std::sin(   x ) + std::sin( 2*x )
+         + std::sin( 3*x ) + std::sin( 4*x );
+  }
+}
+
 
 double range(std::function<double(double)> f,
              double a, double b,
